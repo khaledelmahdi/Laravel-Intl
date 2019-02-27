edc6e8e062af3bcd1b33f27e712fc5ad98097ce0
Propaganistas <Propaganistas@users.noreply.github.com>
2019-02-27 07:31:32 +0100

    L5.8

--- tests/TestNumber.php
+++ tests/TestNumber.php
@@ -15,7 +15,7 @@ class TestNumber extends TestCase
         return [IntlServiceProvider::class];
     }
 
-    public function setUp()
+    public function setUp(): void
     {
         require_once __DIR__.'/../src/helpers.php';
 
